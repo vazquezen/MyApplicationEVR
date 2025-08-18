7a242db3d5ec7d2669917d2eba14ac92b3d9a0fc
Enrique Vázquez Rodríguez <enrique.vr@outlook.es>
2025-08-18 03:05:52 -0600

    feat: Se implementa la opcion de reCaptcha
    Para visualizar la opcione del SDK de reCaptcha
    
    BREAKING CHANGE: Change on app

--- app/src/main/java/com/evr/tes/ui/captchascreen/FriendlyCaptchaActivity.kt
+++ app/src/main/java/com/evr/tes/ui/captchascreen/FriendlyCaptchaActivity.kt
@@ -18,9 +18,11 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,11 +31,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.evr.tes.R
 import com.evr.tes.core_data.friendly.doLoginRequest
 import com.evr.tes.helpers.Keys.FRIENDLY_CAPTCHA_SITEKEY
 import com.evr.tes.ui.success.SuccessActivity
@@ -54,13 +58,20 @@ class FriendlyCaptchaActivity : ComponentActivity() {
         sdk.createWidget(sitekey = FRIENDLY_CAPTCHA_SITEKEY)
     }
 
-    @OptIn(ExperimentalFoundationApi::class)
+    @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    topBar = {
+                    TopAppBar(
+                        title = { Text(text = stringResource(id = R.string.title_activity_friendly_captcha)) }
+                    )
+                },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     LoginForm(
                         modifier = Modifier.padding(innerPadding),
                         onLoginClicked = { username, password, captchaResponse, setLoading, setMessage ->
--- app/src/main/java/com/evr/tes/ui/menuscreen/MenuScreen.kt
+++ app/src/main/java/com/evr/tes/ui/menuscreen/MenuScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.evr.tes.ui.captchascreen.FriendlyCaptchaActivity
+import com.evr.tes.ui.captchascreen.GoogleReCaptchaActivity
 import com.evr.tes.ui.success.SuccessActivity
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -38,7 +39,7 @@ fun MenuScreen(
                     context.startActivity(
                         Intent(
                             context,
-                            SuccessActivity::class.java
+                            GoogleReCaptchaActivity::class.java
                         )
                     )
                 }
